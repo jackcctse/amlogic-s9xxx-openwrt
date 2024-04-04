9753731f76c990373eadcf13c1285b072d4fccea
jackcctse <41234174+jackcctse@users.noreply.github.com>
2024-04-05 00:19:20 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -198,7 +198,7 @@ rebuild_firmware() {
         \
         luci luci-base luci-compat luci-lib-base \
         luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio luci-mod-admin-full luci-mod-network \
-        luci-mod-status luci-mod-system luci-proto-ipip luci-proto-ipv6 luci-proto-ppp \
+        luci-mod-status luci-mod-system luci-proto-ipv6 luci-proto-ppp \
         \
         luci-app-amlogic \
         \
